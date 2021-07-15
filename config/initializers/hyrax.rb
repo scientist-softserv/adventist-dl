8b4bb66943b67ac207c85fbd0e72b3102ca0a292
Rob Kaufman <rob@notch8.com>
2021-07-14 22:56:03 -0700

    use the pathname from env all the time

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -106,14 +106,12 @@ Hyrax.config do |config|
 
   # Temporary path to hold uploads before they are ingested into FCrepo.
   # This must be a lambda that returns a Pathname
-  if Settings.multitenancy.enabled
-   config.upload_path = ->() do
-     if Settings.s3.upload_bucket
-       "uploads/#{Apartment::Tenant.current}"
-     else
-       ENV['HYRAX_UPLOAD_PATH'].present? ? ENV['HYRAX_UPLOAD_PATH'] : Rails.root + 'public' + 'uploads' + Apartment::Tenant.current
-     end
-   end
+  config.upload_path = ->() do
+    if Settings.s3.upload_bucket
+      "uploads/#{Apartment::Tenant.current}"
+    else
+      ENV['HYRAX_UPLOAD_PATH'].present? ? Pathname.new(File.join(ENV['HYRAX_UPLOAD_PATH'], Apartment::Tenant.current)) : Rails.root.join('public', 'uploads', Apartment::Tenant.current)
+    end
   end
 
   # Location on local file system where derivatives will be stored.
