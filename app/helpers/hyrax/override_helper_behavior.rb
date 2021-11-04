97150da0eae3c227e7f78635ed7becf7fadfbb05
Shana Moore <shana.lavina.moore@gmail.com>
2021-11-04 00:58:31 +0000

    Search bug

--- app/helpers/hyrax/override_helper_behavior.rb
+++ app/helpers/hyrax/override_helper_behavior.rb
@@ -36,5 +36,15 @@ module Hyrax
       state.add_facet_params(Solrizer.solr_name(facet.keys.first, :facetable),
                              facet.values.first)
     end
+
+    # A Blacklight index field helper_method
+    # @param [Hash] options from blacklight helper_method invocation. Maps rights statement URIs to links with labels.
+    # @return [ActiveSupport::SafeBuffer] rights statement links, html_safe
+    def rights_statement_links(options)
+      service = Hyrax.config.rights_statement_service_class.new
+      to_sentence(options[:value].map { |right| link_to service.label(right), right })
+    rescue KeyError
+      options[:value]
+    end
   end
 end
