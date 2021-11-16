addaecd2b1b78ae049ace3f7ca98be27fa40e6dc
Rob Kaufman <rob@notch8.com>
2021-11-15 17:11:24 -0800

    works in other collecitons

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -9,6 +9,24 @@ module Bulkrax::HasLocalProcessing
       self.parsed_metadata.delete('admin_set_id')
       nested_collections = self.parsed_metadata.delete('member_of_collections_attributes')
       self.parsed_metadata['collection'] = nested_collections.map { |k, v| v[:id] } if nested_collections.is_a?(Hash)
+
+      if self.parsed_metadata['part_of'].present?
+        self.parsed_metadata['part_of'].each do |collection_title|
+          collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
+          self.parsed_metadata['collection'] ||= []
+          self.parsed_metadata['collection'] << collection.id
+        end
+      end
+    else
+      if self.parsed_metadata['part_of'].present?
+        self.parsed_metadata['part_of'].each do |collection_title|
+          collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
+          self.parsed_metadata['member_of_collections_attributes'] || {}
+          top_key = self.parsed_metadata['member_of_collections_attributes'].keys.map {|k| k.to_i}.sort.last || -1
+          self.parsed_metadata['member_of_collections_attributes'][top_key + 1] = collection.id
+        end
+      end
     end
+
   end
 end
