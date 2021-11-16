00afc9331ea15c5204c4ac708f81da49e8658cbc
Rob Kaufman <rob@notch8.com>
2021-11-15 21:43:29 -0800

    connect issues to periodical collections

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -6,27 +6,46 @@ module Bulkrax::HasLocalProcessing
   # to add a custom property from outside of the import data
   def add_local
     if self.parsed_metadata['model'] == 'Collection'
-      self.parsed_metadata.delete('admin_set_id')
-      nested_collections = self.parsed_metadata.delete('member_of_collections_attributes')
-      self.parsed_metadata['collection'] = nested_collections.map { |k, v| v[:id] } if nested_collections.is_a?(Hash)
-
-      if self.parsed_metadata['part_of'].present?
-        self.parsed_metadata['part_of'].each do |collection_title|
-          collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
-          self.parsed_metadata['collection'] ||= []
-          self.parsed_metadata['collection'] << collection.id
-        end
-      end
+      add_collection_nesting
     else
-      if self.parsed_metadata['part_of'].present?
-        self.parsed_metadata['part_of'].each do |collection_title|
-          collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
-          self.parsed_metadata['member_of_collections_attributes'] || {}
-          top_key = self.parsed_metadata['member_of_collections_attributes'].keys.map {|k| k.to_i}.sort.last || -1
-          self.parsed_metadata['member_of_collections_attributes'][top_key + 1] = collection.id
-        end
+      add_part_of_collections if self.parsed_metadata['part_of'].present?
+      add_set_collections if self.parsed_metadata['resource_type'].include?('Issue')
+    end
+  end
+
+  def add_set_collections
+    sets = record.header.set_spec.map(&:content)
+    aark_ids = sets.select { |s| s.match(/^\d+$/) }
+    aark_ids.each do |aark|
+      collection = Collection.where(aark_id_tesim: aark).first
+      add_collection_to_work(collection) if collection
+    end
+  end
+
+  def add_part_of_collections
+    self.parsed_metadata['part_of'].each do |collection_title|
+      collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
+      add_collection_to_work(collection)
+    end
+  end
+
+  def add_collection_to_work(collection)
+    self.parsed_metadata['member_of_collections_attributes'] || {}
+    top_key = self.parsed_metadata['member_of_collections_attributes'].keys.map {|k| k.to_i}.sort.last || -1
+    self.parsed_metadata['member_of_collections_attributes']["#{top_key + 1}"] = { id: collection.id }
+  end
+
+  def add_collection_nesting
+    self.parsed_metadata.delete('admin_set_id')
+    nested_collections = self.parsed_metadata.delete('member_of_collections_attributes')
+    self.parsed_metadata['collection'] = nested_collections.map { |k, v| v[:id] } if nested_collections.is_a?(Hash)
+
+    if self.parsed_metadata['part_of'].present?
+      self.parsed_metadata['part_of'].each do |collection_title|
+        collection = Collection.where(title_sim: collection_title).first || Collection.create(title: [collection_title], collection_type: Hyrax::CollectionType.find_by(title: 'User Collection'))
+        self.parsed_metadata['collection'] ||= []
+        self.parsed_metadata['collection'] << collection.id
       end
     end
-
   end
 end
