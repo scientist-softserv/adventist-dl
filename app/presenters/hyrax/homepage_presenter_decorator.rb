4f5106e9793d0d37a1da7d028b014f65276855a9
Alisha Evans <alisha@notch8.com>
2021-11-30 17:17:29 -0600

    able to feature flip per tenant whether to show the featured researcher

---
+++ app/presenters/hyrax/homepage_presenter_decorator.rb
@@ -0,0 +1,10 @@
+module Hyrax
+  module HomepagePresenterDecorator
+    # add feature flag for featured researcher
+    def display_featured_researcher?
+      Flipflop.show_featured_researcher?
+    end
+  end
+end
+
+Hyrax::HomepagePresenter.prepend(Hyrax::HomepagePresenterDecorator)
