bc1f2eb36f29ef4dd8e196c9611a1d908d35f761
Rob Kaufman <rob@notch8.com>
2021-12-14 23:21:31 -0800

    allow env to know when we are importing, skip some collection logic if we are importing

---
+++ app/factories/bulkrax/object_factory_decorator.rb
@@ -0,0 +1,12 @@
+module Bulkrax
+  module ObjectFactoryDecorator
+
+    # @param [Hash] attrs the attributes to put in the environment
+    # @return [Hyrax::Actors::Environment]
+    def environment(attrs)
+      Hyrax::Actors::Environment.new(object, Ability.new(@user), attrs, true)
+    end
+  end
+end
+
+::Bulkrax::ObjectFactory.prepend(Bulkrax::ObjectFactoryDecorator)
