32a6af168aa5155595b6e225030d79d2d6d513d1
Rob Kaufman <rob@notch8.com>
2021-12-21 11:29:26 -0800

    rubocop fixes

--- lib/hydra/derivatives/processors/image_decorator.rb
+++ lib/hydra/derivatives/processors/image_decorator.rb
@@ -2,20 +2,24 @@
 
 # Fix PDF tripple issue
 
-module Hydra::Derivatives::Processors
-  module ImageDecorator
+module Hydra
+  module Derivatives
+    module Processors
+      module ImageDecorator
+        protected
 
-    protected
-    # When resizing images, it is necessary to flatten any layers, otherwise the background
-    # may be completely black. This happens especially with PDFs. See #110
-    def create_resized_image
-      create_image do |xfrm|
-        if size
-          xfrm.combine_options do |i|
-            i.flatten
-            i.resize(size)
+          # When resizing images, it is necessary to flatten any layers, otherwise the background
+          # may be completely black. This happens especially with PDFs. See #110
+          def create_resized_image
+            create_image do |xfrm|
+              if size
+                xfrm.combine_options do |i|
+                  i.flatten
+                  i.resize(size)
+                end
+              end
+            end
           end
-        end
       end
     end
   end
