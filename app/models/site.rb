987c3e573769e942b04cd907843640a66703dc22
Sara G <geezy@Notch8-MacBook-Pro.local>
2022-02-03 19:55:30 -0600

    rubocop

--- app/models/site.rb
+++ app/models/site.rb
@@ -29,7 +29,7 @@ class Site < ApplicationRecord
       first_or_create do |site|
         site.available_works = Hyrax.config.registered_curation_concern_types
       end
-    rescue
+    rescue StandardError
       NilSite.instance
     end
   end
