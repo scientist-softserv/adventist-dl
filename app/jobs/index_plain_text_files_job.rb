c6ce5adac1c574f1c275e4b1f2ac78a62667690f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2022-10-26 16:03:01 -0400

    Adding account information for nested job

--- app/jobs/index_plain_text_files_job.rb
+++ app/jobs/index_plain_text_files_job.rb
@@ -13,19 +13,21 @@ class IndexPlainTextFilesJob < ApplicationJob
   # @see https://docs.ruby-lang.org/en/2.7.0/String.html#method-i-encode
   class One < ApplicationJob
     # @param file_set_id [String]
-    def perform(file_set_id)
-      file_set = FileSet.find(file_set_id)
-      file = file_set.original_file
+    def perform(account, file_set_id)
+      account.switch do
+        file_set = FileSet.find(file_set_id)
+        file = file_set.original_file
 
-      unless file
-        raise RuntimeError, "FileSet ID=\"#{file_set_id}\" expected to have an original_file; however it was missing"
+        unless file
+          raise RuntimeError, "FileSet ID=\"#{file_set_id}\" expected to have an original_file; however it was missing"
+        end
+
+        Adventist::TextFileTextExtractionService.assign_extracted_text(
+          file_set: file_set,
+          text: file.content,
+          original_file_name: Array(file.file_name).first
+        )
       end
-
-      Adventist::TextFileTextExtractionService.assign_extracted_text(
-        file_set: file_set,
-        text: file.content,
-        original_file_name: Array(file.file_name).first
-      )
     end
   end
 
@@ -35,7 +37,7 @@ class IndexPlainTextFilesJob < ApplicationJob
       FileSet.where(mime_type_ssi: 'text/plain').find_each do |file_set|
         next if file_set.extracted_text.present?
 
-        One.perform_later(file_set.id)
+        One.perform_later(account, file_set.id)
       end
     end
   end
