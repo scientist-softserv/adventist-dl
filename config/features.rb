0d06951d44aa94922c74e5e7ceee4247d595c5db
DeonFranklin <deondeefranklin@gmail.com>
2022-11-16 11:09:36 -0800

    added feature flipper to enable sharing work

--- config/features.rb
+++ config/features.rb
@@ -2,4 +2,8 @@ Flipflop.configure do
   feature :show_featured_researcher,
           default: false,
           description: "Shows the Featured Researcher tab on the homepage."
-end
+
+          feature :show_share_button,
+          default: true,
+          description: "Shows the 'Share Your Work' button on the homepage."
+ end
