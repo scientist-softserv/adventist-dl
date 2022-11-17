ad0c1f0302c7f2a4d685b3bf1a176a0d5041b8e5
DeonFranklin <deondeefranklin@gmail.com>
2022-11-17 12:43:31 -0800

    added feature to feature.rb file and updated the share button method on the homepage presenter decorator

--- config/features.rb
+++ config/features.rb
@@ -3,7 +3,7 @@ Flipflop.configure do
           default: false,
           description: "Shows the Featured Researcher tab on the homepage."
 
-          feature :show_share_button,
+  feature :show_share_button,
           default: true,
           description: "Shows the 'Share Your Work' button on the homepage."
  end
