018608639f2392888cb14182ecdb35d0149ac705
Shana <shana.lavina.moore@gmail.com>
2023-02-01 14:41:17 -0800

    [i240] - override bulkrax #collections_created method
    ref proposed solution and discussion:
    https://github.com/scientist-softserv/adventist-dl/issues/240#issuecomment-1412456590

--- app/models/bulkrax/oai_adventist_qdc_entry.rb
+++ app/models/bulkrax/oai_adventist_qdc_entry.rb
@@ -7,5 +7,23 @@ module Bulkrax
     def add_thumbnail_url
       true
     end
+
+    def collections_created?
+      true
+    end
+
+    def find_collection_ids
+      return self.collection_ids if collections_created?
+      if sets.blank? || parser.collection_name != 'all'
+        collection = find_collection(importerexporter.unique_collection_identifier(parser.collection_name))
+        self.collection_ids << collection.id if collection.present? && !self.collection_ids.include?(collection.id)
+      else # All - collections should exist for all sets
+        sets.each do |set|
+          c = find_collection(importerexporter.unique_collection_identifier(set.content))
+          self.collection_ids << c.id if c.present? && !self.collection_ids.include?(c.id)
+        end
+      end
+      return self.collection_ids
+    end
   end
 end
