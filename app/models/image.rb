f45ed253f072ca8c95497caf39a2daaa4f77f34c
Kirk Wang <kirk.wang@scientist.com>
2023-02-21 14:36:03 -0800

    Configure IiifPrint, trying to get it to work

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,6 +11,7 @@ class Image < ActiveFedora::Base
   include DogBiscuits::Geo
   include DogBiscuits::PartOf
   include DogBiscuits::PlaceOfPublication
+  include IiifPrint.model_configuration
 
   property :extent, predicate: ::RDF::Vocab::DC.extent, multiple: true do |index|
     index.as :stored_searchable
