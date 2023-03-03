78068740c4ea052908f4dfa319704be037270205
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-03 09:42:47 -0500

    Removing automatic OAI collection creation
    Prior to this commit we were attempting to create collections from the
    OAI feed's metadata.  This has been a source of consistent
    consternation.
    
    With this commit, we're removing the auto-creation and attempts at
    assigning collections to imported records.  Instead the client will
    import OAI records, then export those records via CSV.  They will then
    update the CSV to create the relationships between records and their
    membership in collections.  They will then import that updated CSV into
    Bulkrax.
    
    The current version of Bulkrax includes the [patch][1] that addresses
    exporter memory issues.
    
    Further, the `Bulrkax::HasLocalProcessing` module masked the setting of
    admin_set_id creating an error.  The code removed in this commit was
    previously deleting the `admin_set_id` from the Collection.
    
    Related to:
    
    - https://github.com/scientist-softserv/adventist-dl/issues/281
    
    [1]: https://github.com/samvera-labs/bulkrax/pull/749

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -6,59 +6,7 @@ module Bulkrax
     # add any special processing here, for example to reset a metadata property
     # to add a custom property from outside of the import data
     def add_local
-      if self.parsed_metadata['model'] == 'Collection' || factory_class == Collection
-        add_collection_nesting
-      else
-        add_part_of_collections if self.parsed_metadata['part_of'].present?
-        add_set_collections
-      end
-    end
-
-    def add_set_collections
-      # The #header method is for OAI records
-      return unless record.respond_to?(:header)
-      sets = record.header.set_spec.map(&:content)
-      aark_ids = sets.select { |s| s.match(/^\d+$/) }
-      aark_ids.each do |aark|
-        collection = Collection.where(aark_id_tesim: aark).first
-        add_collection_to_work(collection) if collection
-      end
-    end
-
-    def add_part_of_collections
-      self.parsed_metadata['part_of'].each do |collection_title|
-        collection = find_or_create_collection(collection_title)
-        add_collection_to_work(collection)
-      end
-    end
-
-    def add_collection_to_work(collection)
-      self.parsed_metadata['member_of_collections_attributes'] ||= {}
-      top_key = self.parsed_metadata['member_of_collections_attributes'].keys.map { |k| k.to_i }.sort.last || -1
-      self.parsed_metadata['member_of_collections_attributes'][(top_key + 1).to_s] = { id: collection.id }
-    end
-
-    def add_collection_nesting
-      self.parsed_metadata.delete('admin_set_id')
-      nested_collections = self.parsed_metadata.delete('member_of_collections_attributes')
-      self.parsed_metadata['collection'] = nested_collections.map { |k, v| v[:id] } if nested_collections.is_a?(Hash)
-
-      if self.parsed_metadata['part_of'].present?
-        self.parsed_metadata['part_of'].each do |collection_title|
-          collection = find_or_create_collection(collection_title)
-          self.parsed_metadata[parser.related_parents_parsed_mapping] ||= []
-          self.parsed_metadata[parser.related_parents_parsed_mapping] << collection.id
-        end
-      end
-    end
-
-    def find_or_create_collection(collection_title)
-      Collection.where(title_sim: collection_title).first ||
-        Collection.create(title: [collection_title], collection_type: Bulkrax::HasLocalProcessing.default_collection_type)
-    end
-
-    def self.default_collection_type
-      Hyrax::CollectionType.find_or_create_by(title: 'User Collection')
+      true
     end
   end
 end
