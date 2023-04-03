45844cfed238e6b7272f41c2f53c757ff31a6df7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-04-03 18:27:00 -0400

    Updating Hyrax::DerivativeService.services spec
    This is a provisional update; but one that I think requires
    conversation.  Namely, should the `Hyrax::DerivativeService.services`
    have anything besides `IiifPrint::PluggableDerivativeService`?
    
    As implemented in Hyrax, the first service we encounter that is valid is
    the one that is run.
    
    The `Adventist::TextFileTextExtractionService` only runs against plain
    text files.  However, I believe the
    `IiifPrint::PluggableDerivativeService` wraps the
    `Hyrax::DerivativeService`.
    
    Thoughts?
    
    Related to:
    
    - https://github.com/scientist-softserv/adventist-dl/issues/317

--- spec/services/adventist/text_file_text_extraction_service_spec.rb
+++ spec/services/adventist/text_file_text_extraction_service_spec.rb
@@ -22,9 +22,11 @@ RSpec.describe Adventist::TextFileTextExtractionService do
 
   describe 'position in the array of Hyrax::DerivativeService.services' do
     it "is in the first position" do
-      expect(Hyrax::DerivativeService.services).to match_array(
-        [Adventist::TextFileTextExtractionService, Hyrax::FileSetDerivativesService]
-      )
+  expect(Hyrax::DerivativeService.services).to(
+    match_array(
+      [Adventist::TextFileTextExtractionService,
+       IiifPrint::PluggableDerivativeService,
+       Hyrax::FileSetDerivativesService]))
     end
   end
 
