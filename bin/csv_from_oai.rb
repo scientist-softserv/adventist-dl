59fca5c0f2a0ea25cb33884cfdd150b95058d24f
DeonFranklin <deondeefranklin@gmail.com>
2023-04-16 21:39:16 -0700

    This commit contains the changes to the code to split the derivative types in the CSV.

--- bin/csv_from_oai.rb
+++ bin/csv_from_oai.rb
@@ -4,13 +4,13 @@ require 'oai'
 require 'byebug'
 
 # Input for dynamic email
-# puts "Enter your email address:"
-# email = gets.chomp
+puts "Enter your email address:"
+email = gets.chomp
 
 # OAI client setup
 client = OAI::Client.new(
   "http://oai.adventistdigitallibrary.org/OAI-script",
-  headers: { from: 'deon.franklin@scientist.com' },
+  headers: { from: email },
   parser: 'libxml'
 )
 
@@ -20,7 +20,7 @@ opts = {
 
 CSV.open('csv_from_oai.csv', 'wb') do |csv|
   # Write the headers to the CSV file
-  csv << ['set', 'identifier', 'resource_type', 'URL']
+  csv << ['set', 'identifier', 'derivative_type', 'URL']
 
   sets = ["adl:thesis",
     "adl:periodical",
@@ -28,19 +28,30 @@ CSV.open('csv_from_oai.csv', 'wb') do |csv|
     "adl:article",
     "adl:image",
     "adl:book",
-    "adl:other"]
+    "adl:other"
+  ]
 
-    sets.each do |set|
-      records = client.list_records(opts.merge(set: set))
-      records.each_with_index do |r, i|
-        # begin
-        identifier = r.metadata.first.find('identifier').first&.content
-        resource_type = r.metadata.first.find('resource_type').first&.content
-        related_url = r.metadata.first.find('related_url').first&.content
-        thumbnail_url = r.metadata.first.find('thumbnail_url').first&.content        
-          # Write records to the CSV file
-          csv << [set, identifier, resource_type, related_url || thumbnail_url]
+  sets.each do |set|
+    records = client.list_records(opts.merge(set: set))
+    records.each do |r|
+      identifier = r.header.identifier
+      thumbnail_url = r.metadata.first.find('thumbnail_url').first&.content
+      related_url = r.metadata.first.find('related_url').first&.content
 
+      # Split thumbnail_url and extract file type
+      thumbnail_file_type = thumbnail_url.split('.').last if thumbnail_url
+      # Split related_url and extract file type
+      related_file_type = related_url.split('.').last if related_url
+
+      # Write thumbnail URL to CSV
+      if thumbnail_url
+        csv << [set, identifier, thumbnail_file_type, thumbnail_url]
+      end
+
+      # Write related URL to CSV
+      if related_url
+        csv << [set, identifier, related_file_type, related_url]
+      end
     end
   end
 end
