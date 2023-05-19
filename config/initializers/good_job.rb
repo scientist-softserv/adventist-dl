bb107700aeaff2a8d525394a23adb977c6d09553
Kirk Wang <kirk.wang@scientist.com>
2023-05-19 14:00:58 -0700

    ⚙️ Properly load GoodJob priority config
    This commit will wrap the GoodJob priority config in an after_initialize
    block to ensure that the constants are all loaded before setting the
    priorities.  This will solve uninitialized constant errors that were
    preventing the application from starting.

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -12,19 +12,22 @@ Rails.application.configure do
   # config.good_job.cron = { example: { cron: '0 * * * *', class: 'ExampleJob'  } }
 end
 
-# baseline of 0, higher is sooner
-Bulkrax::ScheduleRelationshipsJob.priority = 50
-CreateDerivativesJob.priority = 40
-CharacterizeJob.priority = 30
-Hyrax::GrantEditToMembersJob.priority = 10
-ImportUrlJob.priority = 10
-IngestJob.priority = 10
-ApplicationJob.priority = 0
-AttachFilesToWorkJob.priority = -1
-Bulkrax::ImportWorkJob.priority = -5
-Bulkrax::ImportFileSetJob.priority = -15
-Bulkrax::CreateRelationshipsJob.priority = -20
-Bulkrax::ImporterJob.priority = -20
-IiifPrint::Jobs::CreateRelationshipsJob.priority = -20
-ContentDepositEventJob.priority = -50
-ContentUpdateEventJob.priority = -50
+# Wrapping this in an after_initialize block to ensure that all constants are loaded
+Rails.application.config.after_initialize do
+  # baseline of 0, higher is sooner
+  Bulkrax::ScheduleRelationshipsJob.priority = 50
+  CreateDerivativesJob.priority = 40
+  CharacterizeJob.priority = 30
+  Hyrax::GrantEditToMembersJob.priority = 10
+  ImportUrlJob.priority = 10
+  IngestJob.priority = 10
+  ApplicationJob.priority = 0
+  AttachFilesToWorkJob.priority = -1
+  Bulkrax::ImportWorkJob.priority = -5
+  Bulkrax::ImportFileSetJob.priority = -15
+  Bulkrax::CreateRelationshipsJob.priority = -20
+  Bulkrax::ImporterJob.priority = -20
+  IiifPrint::Jobs::CreateRelationshipsJob.priority = -20
+  ContentDepositEventJob.priority = -50
+  ContentUpdateEventJob.priority = -50
+end
