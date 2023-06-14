73293ce2a8f9539d4b9c7eb77532d8cb3cbf5af1
Shana Moore <shana@scientist.com>
2023-06-14 14:19:00 -0700

    :recycle: add logging and rescues to hopes of capturing better errors
    To run: rake hyku:destroy_collections['sdapi']
    
    Co-Authored-By: Jeremy Friesen <jeremy.n.friesen@gmail.com>

--- app/jobs/destroy_collections_job.rb
+++ app/jobs/destroy_collections_job.rb
@@ -2,20 +2,14 @@
 
 # app/jobs/destroy_collections_job.rb
 class DestroyCollectionsJob < ApplicationJob
-  def perform
-    # Find specific collection
-    collection_count = Collection.count
-    counter = 0
-
-    Collection.find_each do |collection|
-      next if collection.title.first == 'Journal of Adventist Education'
-
-      # Destroy the collections
+  def perform(collection_id)
+    collection = Collection.find(collection_id)
+    begin
       collection.destroy!
-      counter += 1
-      Rails.logger.info "#{collection.title.first} was destroyed!"
+      Rails.logger.info "Collection Title =#{collection.title.first} was destroyed!"
+    rescue StandardError => e
+      failed_records << collection_id
+      Rails.logger.error "Failed to destroy the collection: #{e.message}."
     end
-
-    Rails.logger.info "#{counter} of #{collection_count} collections were destroyed!"
   end
 end
