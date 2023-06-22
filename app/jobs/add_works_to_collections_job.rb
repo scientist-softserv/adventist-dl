bb84491a22e9a22673172bb72e43024f15a25aaa
Shana Moore <shana@scientist.com>
2023-06-22 07:58:33 -0700

    :broom: save to a persistent volume path

--- app/jobs/add_works_to_collections_job.rb
+++ app/jobs/add_works_to_collections_job.rb
@@ -15,7 +15,7 @@ class AddWorksToCollectionsJob < ApplicationJob
     notice = "😈😈😈 ERROR: unable to create relationship for #{record_data[:identifier]}"
     Rails.logger.error(notice)
     Rails.logger.error(e.message)
-    File.open('lib/data/add_works_to_collection_errors.txt', 'a') do |f|
+    File.open('uploads/add_works_to_collection_errors.txt', 'a') do |f|
       f.puts Time.zone.now
       f.puts notice
       f.puts e.message
