d73a07beb8cb6fdef900b145f66c28ddb1148109
Kirk Wang <kirk.wang@scientist.com>
2023-11-22 14:35:29 -0800

    🐛 Check for pdf extension
    This commit will add a check for the file set's extension (label)
    instead of solely relying on the mime type. This will allow us to know
    if it's a PDF (as best as we can anyway) even if CharacterizeJob did not
    run.
    
    Also, downgraded the version of simple_form because it was causing
    crashes in development.

--- app/helpers/pdf_js_helper.rb
+++ app/helpers/pdf_js_helper.rb
@@ -3,13 +3,13 @@
 module PdfJsHelper
   def pdf_js_url(file_set_presenter)
     # assumes that the download path exists if the file set has been characterized
-    path = if file_set_presenter.mime_type
-             hyrax.download_path(file_set_presenter.id)
-           else
-             file_set_presenter.solr_document["import_url_ssim"].first
-           end
+    url = if file_set_presenter.mime_type
+            "/pdf.js/web/viewer.html?file=#{hyrax.download_path(file_set_presenter.id)}"
+          else
+            file_set_presenter.solr_document["import_url_ssim"].first
+          end
 
-    "/pdf.js/web/viewer.html?file=#{path}##{query_param}"
+    url + "##{query_param}"
   end
 
   def pdf_file_set_presenter(presenter)
@@ -18,16 +18,21 @@ module PdfJsHelper
     # Commenting this line out because even PDFs that were not split will still have a representative media
     # which will be used first in this logic, consider uncommenting once all imports finish
     # representative_presenter(presenter) ||
-    first_file_set_pdf(presenter)
+    external_pdf(presenter)
   end
 
-  def first_file_set_pdf(presenter)
-    pdf_file_set_presenters = presenter.file_set_presenters.select(&:pdf?)
-    reader, archival = pdf_file_set_presenters.partition do |fsp|
+  def external_pdf(presenter)
+    reader, archival = pdf_file_set_presenters(presenter.file_set_presenters).partition do |fsp|
       fsp.solr_document["import_url_ssim"]&.first&.include? "READER"
     end
 
     reader.first || archival.first
+  end
+
+  def pdf_file_set_presenters(presenters)
+    presenters.select(&:pdf?).presence || presenters.select do |file_set_presenter|
+      file_set_presenter.solr_document["label_ssi"].downcase.end_with? ".pdf"
+    end
   end
 
   def representative_presenter(presenter)
--- app/presenters/hyku/work_show_presenter.rb
+++ app/presenters/hyku/work_show_presenter.rb
@@ -31,11 +31,15 @@ module Hyku
 
     def pdf_viewer?
       return unless Flipflop.default_pdf_viewer?
-      return unless file_set_presenters.any?(&:pdf?)
+      return unless file_set_presenters.any?(&:pdf?) || pdf_extension?
 
       # If all of the member_presenters are file_set presenters, return true
       # this also means that there are no child works
       member_presenters.all? { |presenter| presenter.is_a? Hyrax::FileSetPresenter }
+    end
+
+    def pdf_extension?
+      file_set_presenters.any? { |fsp| fsp.label.downcase.end_with?('.pdf') }
     end
 
     def viewer?
