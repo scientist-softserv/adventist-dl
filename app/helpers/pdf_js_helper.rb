65583e333f14d27b277b4633ac14110b50a97e6c
Shana Moore <shana@scientist.com>
2023-11-22 12:21:47 -0800

    ♻️ Add and refactor logic to prioritize pdf types
    In this commit, we add logic to display the correct pdf type when using s3 locations. We should use the READER pdf if one exists, if not, default to what's available (which likely will be ARCHIVAL).

--- app/helpers/pdf_js_helper.rb
+++ app/helpers/pdf_js_helper.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
 module PdfJsHelper
-  def pdf_js_url(path)
+  def pdf_js_url(file_set_presenter)
+    # assumes that the download path exists if the file set has been characterized
+    path = if file_set_presenter.mime_type 
+             hyrax.download_path(file_set_presenter.id)
+           else
+             file_set_presenter.solr_document["import_url_ssim"].first
+           end
+
     "/pdf.js/web/viewer.html?file=#{path}##{query_param}"
   end
 
   def pdf_file_set_presenter(presenter)
     # currently only supports one pdf per work, falls back to the first pdf file set in ordered members
-    representative_presenter(presenter) || presenter.file_set_presenters.select(&:pdf?).first
+    
+    # Commenting this line out because even PDFs that were not split will still have a representative media
+    # which will be used first in this logic, consider uncommenting once all imports finish
+    # representative_presenter(presenter) || 
+    first_file_set_pdf(presenter)
+  end
+
+  def first_file_set_pdf(presenter)
+    pdf_file_set_presenters = presenter.file_set_presenters.select(&:pdf?)
+    reader, archival = pdf_file_set_presenters.partition do |fsp| 
+      fsp.solr_document["import_url_ssim"]&.first&.include? "READER"
+    end
+
+    reader.first || archival.first
   end
 
   def representative_presenter(presenter)
@@ -15,8 +35,6 @@ module PdfJsHelper
   end
 
   def query_param
-    # return unless params[:q]
-
     search_params = current_search_session.try(:query_params) || {}
     q = search_params['q'].presence || ''
 
