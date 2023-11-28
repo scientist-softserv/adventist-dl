3526791488deff42fe653f8bb818b07b2b7e29d2
Kirk Wang <kirk.wang@scientist.com>
2023-11-28 15:16:28 -0800

    🎁 Filter certain file sets from manifests
    This commit will remove thumbnail file sets and non-image file sets at a
    much higher level than before.  Previously, we were filtering out the
    thumbnails at the manifest builder level which is right before the
    manifest gets generated.  This caused issues with serverless IIIF
    resources when the file sets (such as thumbnails or text files) were not
    properly ingested.  This shouldn't break the manifest generation since
    these kinds of file sets are not able to be displayed in the Universal
    Viewer.  Filtering these out at a higher level means it doesn't even
    make it to the serverless IIIF portion of the code in IIIF Print.
    
    Ref:
      - https://github.com/scientist-softserv/adventist-dl/issues/679

---
+++ lib/iiif_manifest/manifest_builder/canvas_builder_factory_decorator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# OVERRIDE IIIFManifest v0.5.0 to remove thumbnail and other non-image files from the manifest
+
+module IIIFManifest
+  module ManifestBuilderDecorator
+    module CanvasBuilderFactoryDecorator
+      THUMBNAIL_FILE_SUFFIX = '.tn.jpg'
+
+      def from(work)
+        composite_builder.new(
+          *file_set_presenters(work).map do |presenter|
+            next if presenter.label.downcase.end_with?(THUMBNAIL_FILE_SUFFIX) || !presenter.image?
+            canvas_builder_factory.new(presenter, work)
+          end
+        )
+      end
+    end
+  end
+end
+
+IIIFManifest::ManifestBuilder::CanvasBuilderFactory
+  .prepend(IIIFManifest::ManifestBuilderDecorator::CanvasBuilderFactoryDecorator)
