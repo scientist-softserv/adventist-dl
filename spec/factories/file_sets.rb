54d7cf8ed278b5fa09ee2cd14ca81f856e660add
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-11-30 14:33:30 -0500

    🚧 Add incomplete FileSetsReprocessJob
    This commit does two things:
    
    1. Extracts a method to answer if we split a given path or not.
    2. Establishes a set of jobs that will perform conditional resplitting
       and/or re-ingesting.
    
    Why the WIP?  I'm heading into an afternoon of meetings and want to put
    something forward that I or another can work on later.
    
    I feel good about merging this as the jobs are not part of production
    code paths.
    
    Related to:
    
    - https://github.com/scientist-softserv/adventist-dl/issues/681

--- spec/factories/file_sets.rb
+++ spec/factories/file_sets.rb
@@ -4,9 +4,23 @@ FactoryBot.define do
   factory :file_set do
     transient do
       user { FactoryBot.create(:user) }
+      content { nil }
     end
+
     after(:build) do |fs, evaluator|
       fs.apply_depositor_metadata evaluator.user
+    end
+
+    factory :file_with_work do
+      after(:build) do |file, _evaluator|
+        file.title = ['testfile']
+      end
+      after(:create) do |file, evaluator|
+        Hydra::Works::UploadFileToFileSet.call(file, evaluator.content) if evaluator.content
+        work = create(:generic_work, user: evaluator.user)
+        work.members << file
+        work.save!
+      end
     end
   end
 end
