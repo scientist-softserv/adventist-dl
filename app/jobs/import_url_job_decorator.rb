3cf162d4e5f18b1aa3b598e2cf2e4140550b239f
Rob Kaufman <rob@notch8.com>
2023-12-01 17:31:26 -0800

    improve the message sent to the mailboxer for failed binarys on fileset import_url_job. this should help with debugging

--- app/jobs/import_url_job_decorator.rb
+++ app/jobs/import_url_job_decorator.rb
@@ -17,7 +17,7 @@ module ImportUrlJobDecorator
         Rails.logger.error(
           %(ImportUrlJob: Error copying <#{uri}> to #{dir} with #{e.message}.  #{e.backtrace.join("\n")})
         )
-        send_error(e.message)
+        send_error(e.message + e.backtrace.join("\n"))
         # TODO: Should we re-raise the exception?  As written this copy_remote_file has a false
         # success.
       end
@@ -26,10 +26,14 @@ module ImportUrlJobDecorator
   end
 
   # OVERRIDE there are calls to send_error that send two arguments.
+  # also give us the fileset with the error
   #
   # @see https://github.com/samvera/hyrax/blob/426575a9065a5dd3b30f458f5589a0a705ad7be2/app/jobs/import_url_job.rb#L76-L105
   def send_error(error_message, *)
-    super(error_message)
+    user = User.find_by_user_key(file_set.depositor)
+    @file_set.errors.add("Error for file_set #{@file_set.id}:", error_message)
+    Hyrax.config.callback.run(:after_import_url_failure, @file_set, user)
+    @operation.fail!(@file_set.errors.full_messages.join(' '))
   end
 end
 
