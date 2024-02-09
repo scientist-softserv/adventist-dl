c5b6999f2f1a63657fa40cd5bc7619750c71a4ef
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-02-08 16:06:00 -0800

    add auxiliary worker to run different queues
    Add a second worker that runs all GoodJob queues. The "base" worker will
    run all queues except the :auxiliary queue.
    
    This, combined with the high priority assigned to all jobs in the
    :auxiliary queue, effectively builds a fast lane; all jobs run in the
    :auxiliary queue will be executed before all other jobs by the auxiliary
    worker.
    
    The fact that the base worker does not run any jobs in the :auxiliary
    queue means we can have this fast lane without putting every other job
    on hold for, potentially, long periods of time.
    
    P.S. The reason we use `before_enqueue` in ApplicationJob is because
    GoodJob does not support setting a default priority per queue.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
+  before_enqueue :set_auxiliary_queue_priority
+
   # limit to 5 attempts
   retry_on StandardError, wait: :exponentially_longer, attempts: 5 do |_job, _exception|
     # Log error, do nothing, etc.
   end
+
+  private
+
+    def set_auxiliary_queue_priority
+      # TODO: make ENV variable
+      self.priority = 100 if queue_name.to_sym == :auxiliary
+    end
 end
